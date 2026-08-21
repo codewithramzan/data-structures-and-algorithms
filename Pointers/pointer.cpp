aaea51bf52b5f6ba53f25bde5c22243f4b1a0a62
Ramzan Khan <ramzanktk2024@gmail.com>
2026-08-21 14:52:20 +0500

    practice pointers

---
+++ Pointers/pointer.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void changeX(int* a){
+	*a = 39;
+}
+
+int main() {
+	// int a = 10;
+	// int* ptr = &a;
+	// int** parPtr = &ptr;
+	// cout<<&a << endl;
+	// cout<< ptr <<endl;
+	// cout<< parPtr <<endl;
+	// cout<< *parPtr <<endl;
+	// cout<< *ptr <<endl;
+	// cout<< **parPtr <<endl;
+	int x = 10 ;
+  int* ptr = &x;
+	cout<<x<<endl;
+
+	changeX(ptr);
+	cout<<x;
+}
