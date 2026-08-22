dbbeb91cbe320d6a694c65de88e2fa4e24a3caae
Ramzan Khan <ramzanktk2024@gmail.com>
2026-08-22 15:04:16 +0500

    find max value in array using pointer

---
+++ Pointers/find_max.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findMax(int* ptr, int n) {
+  int maxVal = *ptr;
+  for(int i = 1; i < n; i++){
+    if(*(ptr+i) > maxVal) {
+      maxVal = *(ptr+i);
+    }
+  }
+
+   return maxVal;
+}
+
+int main() {
+int arr[] = {4, 7, 2, 9, 5};
+int n =sizeof(arr)/sizeof(arr[0]);
+int res = findMax(arr, n);
+cout<< res;
+  return 0;
+}
