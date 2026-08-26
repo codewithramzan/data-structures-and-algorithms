47e5f7272f83f469180c8757bf6db1b3a623c47d
Ramzan Khan <ramzanktk2024@gmail.com>
2026-08-26 14:44:01 +0500

    apply BS using Recursion

--- Searching/binary_search.cpp
+++ Searching/binary_search.cpp
@@ -3,26 +3,40 @@
 
 using namespace std;
 
-int binarySearch(vector<int> arr, int target){
-	int st= 0, end = arr.size() - 1;
-	int mid = st + (end - st)/2;
+// int binarySearch(vector<int> arr, int target){
+// 	int st= 0, end = arr.size() - 1;
+// 	int mid = st + (end - st)/2;
 
-	while (st <= end)
-	{
-		if(target < arr[mid]){//LEFT HALF
-			 end = mid - 1;
-		}else if (target > arr[mid]){//RIGHT HALF
-			 st = mid + 1;
-		} else{
+// 	while (st <= end)
+// 	{
+// 		if(target < arr[mid]){//LEFT HALF
+// 			 end = mid - 1;
+// 		}else if (target > arr[mid]){//RIGHT HALF
+// 			 st = mid + 1;
+// 		} else{
+// 			return mid;
+// 		}
+// 	}
+	
+// 	return -1;
+// }
+int binarySearch(vector<int> arr, int tar, int st, int end) {
+	int mid = st + (end - st)/2;
+	if(st <= end) {
+		if(tar > arr[mid]){
+			return binarySearch(arr,tar,mid+1, end);
+		} else if(tar < arr[mid]){
+			return binarySearch(arr, tar, st, mid-1);
+		} else {
 			return mid;
 		}
 	}
-	
+
 	return -1;
 }
 int main() {
 vector<int> nums = {-1,0,1,3,4,6,7,9};
 
-cout << binarySearch(nums, 9);
+cout << binarySearch(nums, 67, 0, nums.size()-1);
 	return 0;
 }
