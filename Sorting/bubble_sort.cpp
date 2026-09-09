7c18d9fba925c753a0f2d3090985b7c4ea4db317
Ramzan Khan <ramzanktk2024@gmail.com>
2026-09-09 16:38:04 +0500

    we understand and implement selection sort

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -13,6 +13,20 @@ void bubbleSort(int arr[], int n){
 			return;
 		}
 	}
+
+}
+
+
+void selectionSort(int arr[], int n){
+	for(int i=0; i<n-1; i++) {
+		int smallestIdx = i;
+		for(int j= i+1; j < n-1; j++){
+			if(arr[j] < arr[smallestIdx]){
+				smallestIdx =j;
+			}
+		}
+		swap(arr[i], arr[smallestIdx]);
+	}
 }
 void printArray(int arr[], int n){
 	for(int i=0; i<n; i++){
@@ -22,7 +36,7 @@ void printArray(int arr[], int n){
 int main() {
 int arr[] = {5,3,9,6,4,7,8,1};
 int n = sizeof(arr)/sizeof(arr[0]);
-bubbleSort(arr, n);
+selectionSort(arr, n);
 printArray(arr,n);
 	return 0;
 }
